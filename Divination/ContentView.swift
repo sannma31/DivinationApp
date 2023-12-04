cf6f2e7877edfff90df022b146ed0047ae8a7ff1
sannma31 <kasaisyouun2004@gmail.com>
2023-12-05 00:39:54 +0900

    Initial Commit

---
+++ Divination/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Divination
+//
+//  Created by 笠井翔雲 on 2023/12/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Divination/DivinationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DivinationApp.swift
+//  Divination
+//
+//  Created by 笠井翔雲 on 2023/12/05.
+//
+
+import SwiftUI
+
+@main
+struct DivinationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
