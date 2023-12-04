68a42ab1613fd248977c42a02673f02fddef374c
sannma31 <kasaisyouun2004@gmail.com>
2023-12-05 00:55:34 +0900

    add:誕生日画面と現在時間の取得#2

--- Divination/ContentView.swift
+++ Divination/ContentView.swift
@@ -4,7 +4,6 @@
 //
 //  Created by 笠井翔雲 on 2023/12/05.
 //
-
 import SwiftUI
 
 struct YearMonthDay: Hashable {
@@ -29,6 +28,14 @@ struct ContentView: View {
                 .padding(10)
                 .foregroundColor(.gray)
             
+            Text("誕生日を入力してください")
+                .font(.headline)
+            
+            DatePicker("", selection: $birthday.date, displayedComponents: .date)
+                .datePickerStyle(DefaultDatePickerStyle())
+                .labelsHidden()
+                .padding(10)
+            
             Text("血液型を入力してください")
                 .font(.headline)
             
@@ -36,12 +43,36 @@ struct ContentView: View {
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding(10)
                 .foregroundColor(.gray)
+            Text("今日の日付: \(today.year)/\(today.month)/\(today.day)")
+        }
+        .padding()
+        .onAppear {
+            // 今日の日付を取得して設定
+            let todayDate = Date()
+            let calendar = Calendar.current
+            let components = calendar.dateComponents([.year, .month, .day], from: todayDate)
+            today.year = components.year ?? 0
+            today.month = components.month ?? 0
+            today.day = components.day ?? 0
+            
         }
         .padding()
     }
 }
 
-
+extension YearMonthDay {
+    var date: Date {
+        get {
+            return Calendar.current.date(from: DateComponents(year: year, month: month, day: day)) ?? Date()
+        }
+        set {
+            let components = Calendar.current.dateComponents([.year, .month, .day], from: newValue)
+            year = components.year ?? 0
+            month = components.month ?? 0
+            day = components.day ?? 0
+        }
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
