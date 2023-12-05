e553a169d6044cc8c59ada23ac39ccc06587ec4d
sannma31 <kasaisyouun2004@gmail.com>
2023-12-05 23:30:35 +0900

    add:画面遷移

--- Divination/ContentView.swift
+++ Divination/ContentView.swift
@@ -19,45 +19,55 @@ struct ContentView: View {
     @State private var today: YearMonthDay = YearMonthDay(year: 2000, month: 1, day: 27)
     
     var body: some View {
-        VStack(spacing: 20) {
-            Text("名前を入力してね")
-                .font(.headline)
-            
-            TextField("名前を入力してください", text: $name)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(10)
-                .foregroundColor(.gray)
-            
-            Text("誕生日を入力してください")
-                .font(.headline)
-            
-            DatePicker("", selection: $birthday.date, displayedComponents: .date)
-                .datePickerStyle(DefaultDatePickerStyle())
-                .labelsHidden()
-                .padding(10)
-            
-            Text("血液型を入力してください")
-                .font(.headline)
-            
-            TextField("血液型を入力してください", text: $bloodType)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(10)
-                .foregroundColor(.gray)
-            Text("今日の日付: \(today.year)/\(today.month)/\(today.day)")
+        NavigationView{
+            VStack(spacing: 20) {
+                Text("名前を入力してね")
+                    .font(.headline)
+                
+                TextField("名前を入力してください", text: $name)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(10)
+                    .foregroundColor(.gray)
+                
+                Text("誕生日を入力してください")
+                    .font(.headline)
+                
+                DatePicker("", selection: $birthday.date, displayedComponents: .date)
+                    .datePickerStyle(DefaultDatePickerStyle())
+                    .labelsHidden()
+                    .padding(10)
+                
+                Text("血液型を入力してください")
+                    .font(.headline)
+                
+                TextField("血液型を入力してください", text: $bloodType)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(10)
+                    .foregroundColor(.gray)
+                Text("今日の日付: \(today.year)/\(today.month)/\(today.day)")
+                
+                    .padding()
+                    .onAppear {
+                        // 今日の日付を取得して設定
+                        let todayDate = Date()
+                        let calendar = Calendar.current
+                        let components = calendar.dateComponents([.year, .month, .day], from: todayDate)
+                        today.year = components.year ?? 0
+                        today.month = components.month ?? 0
+                        today.day = components.day ?? 0
+                        
+                    }
+                    .padding()
+                NavigationLink(
+                    destination: DivinationApp(name: $name, birthday: $birthday, bloodType: $bloodType, today: $today)){
+                        Text("診断")
+                    }
+                
+                    .buttonStyle(BorderlessButtonStyle()) // Use BorderlessButtonStyle to remove the link appearance
+            }
         }
-        .padding()
-        .onAppear {
-            // 今日の日付を取得して設定
-            let todayDate = Date()
-            let calendar = Calendar.current
-            let components = calendar.dateComponents([.year, .month, .day], from: todayDate)
-            today.year = components.year ?? 0
-            today.month = components.month ?? 0
-            today.day = components.day ?? 0
-            
-        }
-        .padding()
     }
+    
 }
 
 extension YearMonthDay {
--- Divination/DivinationApp.swift
+++ Divination/DivinationApp.swift
@@ -7,11 +7,48 @@
 
 import SwiftUI
 
+
+struct DivinationApp: View {
+    @Binding var name: String
+    @Binding var birthday: YearMonthDay
+    @Binding var bloodType: String
+    @Binding var today: YearMonthDay
+    
+    init(name: Binding<String>, birthday: Binding<YearMonthDay>, bloodType: Binding<String>, today: Binding<YearMonthDay>) {
+        _name = name
+        _birthday = birthday
+        _bloodType = bloodType
+        _today = today
+    }
+    
+    var body: some View {
+        VStack {
+            Text("診断結果じゃ")
+                .font(.title)
+                .padding()
+            
+            // Add your divination results here based on the provided inputs
+            Text("名前: \(name)")
+            Text("誕生日: \(birthday.year)/\(birthday.month)/\(birthday.day)")
+            Text("血液型: \(bloodType)")
+            Text("日付: \(today.year)/\(today.month)/\(today.day)")
+            
+            // Add more divination results as needed
+            
+            Spacer()
+        }
+        .padding()
+    }
+}
+
 @main
-struct DivinationApp: App {
+struct Divinationapp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+
+
